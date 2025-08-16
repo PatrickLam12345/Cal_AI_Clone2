e190f395c69df6eff1fa4e67f85d2ab2e8723ec4
Patrick Lam <patricklam12345@gmail.com>
2025-08-16 00:05:28 -0400

    app bundle release for closed testing (prolly production rdy if no bugs)

--- my_app/android/app/build.gradle.kts
+++ my_app/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_app"
+    namespace = "com.pati.pati"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -29,7 +29,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.my_app"
+        applicationId = "com.pati.pati"
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -37,21 +37,22 @@ android {
         multiDexEnabled = true
     }
 
-  signingConfigs {
-        create("release") {
-            if (keystorePropertiesFile.exists()) {
-                storeFile = file(keystoreProperties["storeFile"] as String)
-                storePassword = keystoreProperties["storePassword"] as String
-                keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
-            }
-        }
-    }
+  // signingConfigs {
+    //     create("release") {
+    //         if (keystorePropertiesFile.exists()) {
+    //             storeFile = file(keystoreProperties["storeFile"] as String)
+    //             storePassword = keystoreProperties["storePassword"] as String
+    //             keyAlias = keystoreProperties["keyAlias"] as String
+    //             keyPassword = keystoreProperties["keyPassword"] as String
+    //         }
+    //     }
+    // }
     buildTypes {
         getByName("release") {
             // Enable both minification and resource shrinking for optimal APK size
             isMinifyEnabled = true
             isShrinkResources = true
+            // signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- my_app/android/app/src/main/kotlin/com/pati/pati/MainActivity.kt
+++ my_app/android/app/src/main/kotlin/com/pati/pati/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.my_app
+package com.pati.pati
 
 import io.flutter.embedding.android.FlutterActivity
 
